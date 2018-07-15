e80bd84efb0349e12f6ffca92a9b3bae8765d99f
Drake Bott <bott.da@gmail.com>
2018-07-15 10:25:55 -0600

    Add delete button function through event delegation

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
 var titleInput = $('.title-input');
 var bodyInput = $('.body-input');
 var saveInput = $('.save-input');
-var ideaCounter = 0;
-
+var ideaIndex = [];
 
 function idea(title, body, quality) {
   this.title = title;
   this.body = body;
   this.quality = 'swill';
-  this.number = ideaCounter;
   this.html = `
-    <article id=${this.number}>
+    <article>
       <h2>${this.title}</h2>
       <button class="delete">Delete</button>
       <p>${this.body}</p>
@@ -20,10 +18,7 @@ function idea(title, body, quality) {
 }
 
 
-
-
-
-$('.save-input').on('click', function() {
+saveInput.on('click', function() {
   event.preventDefault();
   var makeThought = new idea(titleInput.val(), bodyInput.val());
   console.log(makeThought.title);
@@ -33,6 +28,14 @@ $('.save-input').on('click', function() {
   $('.thought').prepend(makeThought.html);
   titleInput.val('');
   bodyInput.val('');
-  ideaCounter ++;
+  ideaIndex.push(makeThought);
 });
 
+$('.thought').on('click', function(e) {
+  var target = $(e.target);
+  if (target.hasClass('delete')){
+    target.parent().remove();
+  }
+  if (target.hasClass('upvote')){
+    }
+});
